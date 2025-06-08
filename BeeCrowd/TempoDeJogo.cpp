d02691ce90de54330973c5c6c04d08e9fc6fa79c
kiyoshi <kiyoshii@protonmail.com>
2025-06-07 22:54:50 -0300

    beecrowd

---
+++ BeeCrowd/TempoDeJogo.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+int main(){
+    int inicio, fim, hora;
+
+    std::cin >> inicio;
+    std::cin >> fim;
+
+    if(inicio < fim){
+        hora = fim - inicio;
+    }
+    else if(fim < inicio){
+        hora = 24 - inicio + fim;
+    }
+    else{
+        hora = 24;
+    }
+    std::cout << "O JOGO DUROU " << hora << " HORA(S)" << '\n';
+
+    return 0;
+}
+
